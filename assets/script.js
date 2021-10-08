b1fe350113f85bfd54b97e1a30864763e029ba3d
ysnrtki <80407792+ysnrtki@users.noreply.github.com>
2021-10-08 15:08:45 +0900

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,37 @@
+﻿$(() => {
+	$("textarea[name='変換元'], select[name='変換方法']").on("blur change", function () {
+		const source = $("textarea[name='変換元']").val() || "";
+		const method = $("select[name='変換方法']").val();
+		if (!method) {
+			return;
+		}
+		if (method === "大文字") {
+			$("textarea[name='変換後']").val(source.toUpperCase());
+		}
+		if (method === "小文字") {
+			$("textarea[name='変換後']").val(source.toLowerCase());
+		}
+		if (method === "コンパクト") {
+			let result = source;
+			result = result.replace(/    /g, "\t");
+			result = result.replace(/ +/g, " ");
+			result = result.replace(/[ \t]+$/g, "");
+			const faweaf = result.split("");
+			result = "";
+			for (let i = 0; i < faweaf.length; i++) {
+				if (i >= 2 && /^\r?\n$/.test(faweaf[i]) && /^\r?\n$/.test(faweaf[i - 1]) && /^\r?\n$/.test(faweaf[i - 2])) {
+					continue;
+				}
+				result += faweaf[i];
+			}
+			$("textarea[name='変換後']").val(result);
+		}
+	});
+});
+
+const trimEx = value => {
+	if (typeof value === "string") {
+		return value.replace(/^[\s　]+|[\s　]+$/g, "");
+	}
+	return value;
+};
