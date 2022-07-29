15d73803462397e32053b2ab6d936b1c0e3d10da
ysnrtki <80407792+ysnrtki@users.noreply.github.com>
2022-07-29 11:31:54 +0900

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -7,23 +7,20 @@ $(() => {
 		if (!method) {
 			return;
 		}
-		const 大文字 = () => $("textarea[name='変換後']").val(before.toUpperCase());
-		const 小文字 = () => $("textarea[name='変換後']").val(before.toLowerCase());
-		const コンパクト = () => {
-			let after = trimEx(before);
-			after = after.replace(/    /g, "\t");
-			after = after.replace(/ +/g, " ");
-			after = after.replace(/[ \t]+(\r?\n)/g, "$1");
-			const splitted = after.split("");
-			after = "";
-			for (let i = 0; i < splitted.length; i++) {
-				if (i >= 2 && /^\r?\n$/.test(splitted[i]) && /^\r?\n$/.test(splitted[i - 1]) && /^\r?\n$/.test(splitted[i - 2])) {
-					continue;
+		const 大文字 = str => str.toUpperCase();
+		const 小文字 = str => str.toLowerCase();
+		const コンパクト = str => {
+			let result = trimEx(str);
+			result = result.split(/\r?\n/).map(line => {
+				let indent = "";
+				{
+					const indentedPattern = /^([　\t]+|( {4})+)(.+)/;
+					if (indentedPattern.test(line)) {
+						indent = line.replace(indentedPattern, "$1").replace(/ {4}/g, "\t");
+						line = line.replace(indentedPattern, "$3");
+					}
 				}
-				after += splitted[i];
-			}
-			{
-				after = after.replace(/[！-～]/g, str => String.fromCharCode(str.charCodeAt(0) - 0xFEE0));
+				line = line.replace(/[！-～]/g, str => String.fromCharCode(str.charCodeAt(0) - 0xFEE0));
 				const map = {
 					"’": "'",
 					"－": "-",
@@ -36,26 +33,27 @@ $(() => {
 					"”": "\"",
 					"￥": "\\",
 				};
-				Object.keys(map).forEach(key => after = after.replace(key, map[key]));
-			}
-			$("textarea[name='変換後']").val(after);
+				Object.keys(map).forEach(key => line = line.replace(new RegExp(key, "g"), map[key]));
+				line = trimEx(line.replace(/\s+/g, " ").replace(/ +/g, " "));
+				return `${indent}${line}`;
+			}).join("\n");
+			result = result.replace(/\n{3,}/g, "\n\n");
+			return result;
 		};
 		if (method === "大文字") {
-			大文字();
+			$("textarea[name='変換後']").val(大文字(before));
 		}
 		if (method === "小文字") {
-			小文字();
+			$("textarea[name='変換後']").val(小文字(before));
 		}
 		if (method === "コンパクト") {
-			コンパクト();
+			$("textarea[name='変換後']").val(コンパクト(before));
 		}
 		if (method === "大文字 + コンパクト") {
-			大文字();
-			コンパクト();
+			$("textarea[name='変換後']").val(コンパクト(大文字(before)));
 		}
 		if (method === "小文字 + コンパクト") {
-			小文字();
-			コンパクト();
+			$("textarea[name='変換後']").val(コンパクト(小文字(before)));
 		}
 	}).filter(":first").blur();
 	$("textarea[name='変換前'], textarea[name='変換後']").on("focus", function () {
